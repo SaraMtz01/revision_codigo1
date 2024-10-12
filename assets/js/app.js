4f6471fc58129dd76568c15c8c1b635e3bb9acf0
Sara Martínez <sara.abi.mr@gmail.com>
2024-10-12 12:18:21 -0600

    fix: revisión y corección del código de otra persona. Se modificó el html y el archivo js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,29 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
-const $l = document.querySelector('.location');
+//El nombre de las variables se cambió para mejorar la legibilidad
+const nameParagraph = document.querySelector('.name'); //Al tratarse de una clase y de querySelector se requiere usar un "." antes del nombre de la clase
+const blogParagraph = document.querySelector('.blog'); // se trata de una clase y no de un id, por lo que se requiere usar "."
+const locationParagraph = document.querySelector('.location');
 
-function displayUser(username) {
-  $n.textContent = 'cargando...';
-  const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
-}
-
-function handleError(err) {
+//Se transforma a arrow function ya que requiere de un async
+const displayUser = async (username) =>{
+    const url= `${usersEndpoint}/${username}`; // se agrega una nueva variable que contenga la url para mejorar legibilidad
+    nameParagraph.textContent = 'cargando...';
+    const response = await fetch(url);
+    const data =await response.json();
+    console.log(data);
+    // se coloca acento invertido en lugar de una comilla para el correcto uso de template literals
+    // se coloca innerHTML en lugar de text content para no pasar solo strings
+    nameParagraph.innerHTML = `${data.name}`; 
+    blogParagraph.innerHTML = `${data.blog}`;
+    locationParagraph.innerHTML = `${data.location}`;
+  } 
+  
+  const handleError = (err) =>{
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
-}
+  nameParagraph.innerHTML = `Algo salió mal: ${err}`; //se coloca inner html para que no el contenido no se pase como solo strings
+  }
 
-displayUser('stolinski').catch(handleError);
+
+displayUser("stolinski").catch(handleError);
